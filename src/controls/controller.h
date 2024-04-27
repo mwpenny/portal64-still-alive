26b374e09cc185655ed190d686af776bc0b56df5
GiacomoGarbin <giacomogarbinmailbox@gmail.com>
2024-04-27 15:27:04 +0200

    Use enums instead of defines for constant values.

--- src/controls/controller.h
+++ src/controls/controller.h
@@ -8,20 +8,22 @@
 // file for each library implementation and choose the appropriate one at build
 // time.
 // NOTE: libdragon uses the same values but reversed - see joypad_buttons_t.
-#define BUTTON_A       0x8000
-#define BUTTON_B       0x4000
-#define BUTTON_Z       0x2000
-#define BUTTON_START   0x1000
-#define BUTTON_UP      0x0800
-#define BUTTON_DOWN    0x0400
-#define BUTTON_LEFT    0x0200
-#define BUTTON_RIGHT   0x0100
-#define BUTTON_L       0x0020
-#define BUTTON_R       0x0010
-#define BUTTON_C_UP    0x0008
-#define BUTTON_C_DOWN  0x0004
-#define BUTTON_C_LEFT  0x0002
-#define BUTTON_C_RIGHT 0x0001
+typedef enum {
+    BUTTON_A       = 0x8000,
+    BUTTON_B       = 0x4000,
+    BUTTON_Z       = 0x2000,
+    BUTTON_START   = 0x1000,
+    BUTTON_UP      = 0x0800,
+    BUTTON_DOWN    = 0x0400,
+    BUTTON_LEFT    = 0x0200,
+    BUTTON_RIGHT   = 0x0100,
+    BUTTON_L       = 0x0020,
+    BUTTON_R       = 0x0010,
+    BUTTON_C_UP    = 0x0008,
+    BUTTON_C_DOWN  = 0x0004,
+    BUTTON_C_LEFT  = 0x0002,
+    BUTTON_C_RIGHT = 0x0001,
+} buttons_t;
 
 void controllersInit();
 void controllersSavePreviousState();
