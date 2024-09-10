0d2cd6efe0075f50e927a5cdd5367a769daf60f5
Matt Penny <mwpenny@users.noreply.github.com>
2024-09-09 23:34:16 -0400

    Work on CMake conversion: sound transformation

--- tools/jsox.js
+++ tools/jsox.js
@@ -1,14 +1,28 @@
-const fs = require('fs');
 const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-const fileContents = fs.readFileSync(process.argv[2]);
+if (process.argv.length !== 5) {
+    console.log('Converts sound files using sox and a JSON file containing arguments.\n');
+    console.log(`Usage: ${process.argv[0]} ${process.argv[1]} JSOX_FILE INPUT_FILE OUTPUT_FILE`);
+    process.exit(1);
+}
+
+const [argsFile, inputFile, outputFile] = process.argv.slice(2);
+
+const fileContents = fs.readFileSync(argsFile);
 const fileJSON = JSON.parse(fileContents);
 
 fileJSON.forEach((command) => {
-    const commandText = `sox ${process.argv[3]} ${command.flags || ''} ${process.argv[4]} ${command.filters || ''}`;
+    const commandText = `sox ${inputFile} ${command.flags || ''} ${outputFile} ${command.filters || ''}`;
 
-    process.stdout.write(commandText);
-    process.stdout.write('\n');
+    const outputParentDir = path.dirname(outputFile);
+    if (!fs.existsSync(outputParentDir)) {
+        fs.mkdirSync(outputParentDir, { recursive: true });
+    }
+
+    //process.stdout.write(commandText);
+    //process.stdout.write('\n');
     const script = child_process.exec(commandText);
 
     script.stdout.on('data', function(data){
