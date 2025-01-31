c3639bc0bdc62518077eda50f88171fc1d8ce16f
Matt Penny <mwpenny@users.noreply.github.com>
2025-01-30 23:15:24 -0500

    Lasers travel through portals

--- src/scene/laser.c
+++ src/scene/laser.c
@@ -1,5 +1,7 @@
 #include "laser.h"
 
+#include <assert.h>
+
 #include "defs.h"
 #include "dynamic_scene.h"
 #include "graphics/color.h"
@@ -13,26 +15,18 @@
 
 static struct Coloru8 laserColor = { 255, 0, 0, 100 };
 
-static void laserRender(void* data, struct RenderScene* renderScene, struct Transform* fromView) {
-    struct Laser* laser = (struct Laser*)data;
-
+static void laserBeamBuildVtx(Vtx* vtx, struct LaserBeam* beam, struct Vector3* cameraPosition) {
     // Determine screen-space up direction for billboard
-    struct Vector3 laserForward;
-    vector3Sub(&laser->endPosition, &laser->startPosition, &laserForward);
-
     struct Vector3 tmp;
     struct Vector3 laserUp;
-    vector3Sub(&laser->startPosition, &fromView->position, &tmp);
-    vector3Cross(&laserForward, &tmp, &laserUp);
+    vector3Sub(&beam->startPosition.origin, cameraPosition, &tmp);
+    vector3Cross(&beam->startPosition.dir, &tmp, &laserUp);
     vector3Normalize(&laserUp, &laserUp);
 
     // Create vertices for quad
-    Vtx* vertices = renderStateRequestVertices(renderScene->renderState, 4);
-    Vtx* curr = vertices;
-
     struct Vector3* vertexOrigin;
-    for (int i = 0; i < 4; ++i, ++curr) {
-        vertexOrigin = (i >> 1) ? &laser->endPosition : &laser->startPosition;
+    for (int i = 0; i < 4; ++i, ++vtx) {
+        vertexOrigin = (i >> 1) ? &beam->endPosition : &beam->startPosition.origin;
         vector3AddScaled(
             vertexOrigin,
             &laserUp,
@@ -40,77 +34,115 @@ static void laserRender(void* data, struct RenderScene* renderScene, struct Tran
             &tmp
         );
 
-        curr->v.ob[0] = tmp.x * SCENE_SCALE;
-        curr->v.ob[1] = tmp.y * SCENE_SCALE;
-        curr->v.ob[2] = tmp.z * SCENE_SCALE;
+        vtx->v.ob[0] = tmp.x * SCENE_SCALE;
+        vtx->v.ob[1] = tmp.y * SCENE_SCALE;
+        vtx->v.ob[2] = tmp.z * SCENE_SCALE;
 
-        curr->v.flag = 0;
-        curr->v.tc[0] = 0;
-        curr->v.tc[1] = 0;
+        vtx->v.flag = 0;
+        vtx->v.tc[0] = 0;
+        vtx->v.tc[1] = 0;
 
-        curr->v.cn[0] = laserColor.r;
-        curr->v.cn[1] = laserColor.g;
-        curr->v.cn[2] = laserColor.b;
-        curr->v.cn[3] = laserColor.a;
+        vtx->v.cn[0] = laserColor.r;
+        vtx->v.cn[1] = laserColor.g;
+        vtx->v.cn[2] = laserColor.b;
+        vtx->v.cn[3] = laserColor.a;
+    }
+}
+
+static void laserRender(void* data, struct RenderScene* renderScene, struct Transform* fromView) {
+    struct Laser* laser = (struct Laser*)data;
+    if (laser->beamCount == 0) {
+        return;
     }
 
-    // Render quad
-    Gfx* displayList = renderStateAllocateDLChunk(renderScene->renderState, 3);
+    // Build quads
+    Vtx* vertices = renderStateRequestVertices(renderScene->renderState, laser->beamCount * 4);
+    Vtx* curr = vertices;
+    for (short i = 0; i < laser->beamCount; ++i, curr += 4) {
+        laserBeamBuildVtx(curr, &laser->beams[i], &fromView->position);
+    }
+
+    // Render quads
+    Gfx* displayList = renderStateAllocateDLChunk(renderScene->renderState, laser->beamCount + 2);
     Gfx* dl = displayList;
 
-    gSPVertex(dl++, vertices, 4, 0);
-    gSP1Quadrangle(
-        dl++,
-        0, 1, 3, 2,
-        0
-    );
+    assert(LASER_MAX_BEAMS <= 8);
+    gSPVertex(dl++, vertices, 4 * laser->beamCount, 0);
+
+    for (short i = 0; i < laser->beamCount; ++i) {
+        short relativeVertex = i << 2;
+        gSP1Quadrangle(
+            dl++,
+            relativeVertex,
+            relativeVertex + 1,
+            relativeVertex + 3,
+            relativeVertex + 2,
+            0
+        );
+    }
+
     gSPEndDisplayList(dl++);
 
-    renderSceneAdd(renderScene, displayList, NULL, LASER_INDEX, &laser->startPosition, NULL);
+    renderSceneAdd(renderScene, displayList, NULL, LASER_INDEX, &laser->parent->position, NULL);
 }
 
 void laserInit(struct Laser* laser, struct Transform* parent, struct Vector3* offset) {
     laser->parent = parent;
     laser->parentOffset = *offset;
-    laser->startPosition = gZeroVec;
-    laser->endPosition = gZeroVec;
+    laser->beamCount = 0;
 
     // The laser could traverse multiple rooms and pass through portals.
     //
-    // To avoid a dynamic object per fragment, this single object renders
-    // them all, and so efficiently deriving a meaningful culling radius is
-    // difficult. Given typical turret gameplay and the low rendering cost,
-    // rely solely on room flags.
+    // To avoid a dynamic object per beam, this single object renders them all,
+    // and so efficiently deriving a meaningful culling radius is difficult.
+    // Given typical turret gameplay and the low rendering cost (each beam is
+    // just a quad), rely solely on room flags.
     laser->dynamicId = dynamicSceneAddViewDependant(laser, laserRender, &laser->parent->position, 1000000.0f);
 }
 
 void laserUpdate(struct Laser* laser, int currentRoom) {
-    struct Ray ray = {
-        .origin = laser->parentOffset,
-        .dir    = gForward
-    };
-    rayTransform(laser->parent, &ray, &ray);
+    struct Vector3* startPosition = &laser->parentOffset;
+    struct Transform startTransform = *laser->parent;
+    uint64_t beamRooms = 0;
 
-    laser->startPosition = ray.origin;
+    laser->beamCount = 0;
 
-    struct RaycastHit hit;
-    if (!collisionSceneRaycast(
-            &gCollisionScene,
-            currentRoom,
-            &ray,
-            LASER_COLLISION_LAYERS,
-            1000000.0f,
-            0,
-            &hit)
-    ) {
-        laser->endPosition = laser->startPosition;
-    } else {
-        laser->endPosition = hit.at;
+    for (short i = 0; i < LASER_MAX_BEAMS; ++i) {
+        struct LaserBeam* beam = &laser->beams[i];
+        beam->startPosition.origin = *startPosition;
+        beam->startPosition.dir = gForward;
+        rayTransform(&startTransform, &beam->startPosition, &beam->startPosition);
+
+        struct RaycastHit hit;
+        if (!collisionSceneRaycast(
+                &gCollisionScene,
+                currentRoom,
+                &beam->startPosition,
+                LASER_COLLISION_LAYERS,
+                1000000.0f,
+                0,
+                &hit)
+        ) {
+            break;
+        }
+
+        beam->endPosition = hit.at;
+        beamRooms |= hit.passedRooms;
+        ++laser->beamCount;
+
+        int touchingPortals = collisionSceneIsTouchingPortal(&hit.at, &hit.normal);
+        if (!touchingPortals) {
+            break;
+        } else {
+            int portalIndex = (touchingPortals & RigidBodyIsTouchingPortalA) ? 0 : 1;
+            collisionSceneGetPortalTransform(portalIndex, &startTransform);
+
+            startPosition = &beam->endPosition;
+            currentRoom = gCollisionScene.portalRooms[1 - portalIndex];
+        }
     }
 
-    dynamicSceneSetRoomFlags(laser->dynamicId, hit.passedRooms);
-
-    // TODO: pass through portals, etc.
+    dynamicSceneSetRoomFlags(laser->dynamicId, beamRooms);
 }
 
 void laserRemove(struct Laser* laser) {
--- src/scene/laser.h
+++ src/scene/laser.h
@@ -1,15 +1,23 @@
 #ifndef __LASER_H__
 #define __LASER_H__
 
+#include "math/ray.h"
 #include "math/vector3.h"
+
+#define LASER_MAX_BEAMS 4
+
+struct LaserBeam {
+    struct Ray startPosition;
+    struct Vector3 endPosition;
+};
 
 struct Laser {
     struct Transform* parent;
     struct Vector3 parentOffset;
     short dynamicId;
 
-    struct Vector3 startPosition;
-    struct Vector3 endPosition;
+    struct LaserBeam beams[LASER_MAX_BEAMS];
+    short beamCount;
 };
 
 void laserInit(struct Laser* laser, struct Transform* parent, struct Vector3* offset);
