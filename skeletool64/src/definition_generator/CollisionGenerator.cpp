ba4d2005290c890448460374e19ca38edfbca7dc
Matt Penny <mwpenny@users.noreply.github.com>
2026-07-28 00:56:20 -0400

    Add turret sight collision layer to mesh collider defaults, to match level collision

--- skeletool64/src/definition_generator/CollisionGenerator.cpp
+++ skeletool64/src/definition_generator/CollisionGenerator.cpp
@@ -100,9 +100,9 @@ std::shared_ptr<CollisionGeneratorOutput> generateCollision(const aiScene* scene
             collisionObject->AddPrimitive<const char*>("NULL");
             collisionObject->AddPrimitive<const char*>("NULL");
             collisionObject->Add(std::unique_ptr<DataChunk>(new StructureDataChunk(collider.BoundingBox())));
-            collisionObject->AddPrimitive<const char*>(isTransparent ? 
-                "COLLISION_LAYERS_STATIC | COLLISION_LAYERS_BLOCK_BALL | COLLISION_LAYERS_TRANSPARENT | COLLISION_LAYERS_TANGIBLE" : 
-                "COLLISION_LAYERS_STATIC | COLLISION_LAYERS_BLOCK_BALL | COLLISION_LAYERS_TANGIBLE");
+            collisionObject->AddPrimitive<const char*>(isTransparent ?
+                "COLLISION_LAYERS_STATIC | COLLISION_LAYERS_BLOCK_BALL | COLLISION_LAYERS_TANGIBLE | COLLISION_LAYERS_BLOCK_TURRET_SIGHT | COLLISION_LAYERS_TRANSPARENT" :
+                "COLLISION_LAYERS_STATIC | COLLISION_LAYERS_BLOCK_BALL | COLLISION_LAYERS_TANGIBLE | COLLISION_LAYERS_BLOCK_TURRET_SIGHT");
             collisionObjectChunk->Add(std::move(collisionObject));
 
 
